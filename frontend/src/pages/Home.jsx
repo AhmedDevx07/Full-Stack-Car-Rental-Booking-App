9ebcc6d893d5770ccea1ff300ee6edc884ccd91a
Ahmed-Devx <ahmedoffice69@gmail.com>
2026-05-12 23:14:04 +0500

    Home Page Add

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "../components/Hero";
+import FeaturedSection from "../components/featuredSection";
+import Banner from "../components/Banner";
+import Testimonials from "../components/Testimonials";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <FeaturedSection />
+      <Banner/>
+      <Testimonials/>
+    </>
+  );
+};
+
+export default Home;

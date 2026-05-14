f9a5a94ccf7ee4d9f285a056b132b95f737f8f67
Ahmed-Devx <ahmedoffice69@gmail.com>
2026-05-15 01:42:07 +0500

    Add Middleware Files

---
+++ backend/middleware/auth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const protect = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      return res.json({ success: false, message: "not authorized" });
+    }
+
+    const userId = jwt.decode(token, process.env.JWT_SECRET);
+
+    if (!userId) {
+      return res.json({ success: false, message: "not authorized" });
+    }
+
+    req.user = await User.findById(userId).select("-password");
+    next();
+  } catch (error) {
+    return res.json({ success: false, message: "not authorized" });
+  }
+};

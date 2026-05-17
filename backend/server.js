1ea345345123f7183e1d2caacec0991861ccff71
Ahmed-Devx <ahmedoffice69@gmail.com>
2026-05-17 14:15:08 +0500

    Update Cors Middleware in Server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ const app = express();
 // Connect Database
 await connectDB();
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://car-rental-booking-app-alpha.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/user", userRouter);
